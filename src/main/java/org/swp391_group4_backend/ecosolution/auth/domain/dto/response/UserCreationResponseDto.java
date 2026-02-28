71bdb3451157477cf8bb56faff7886ac904d1c42
AnhNDSE190393 <evosnda34@gmail.com>
2026-02-28 22:23:42 +0700

    configure: change hardcode values to enviroment variables and delete not yet develope module

--- src/main/java/org/swp391_group4_backend/ecosolution/auth/domain/dto/response/UserCreationResponseDto.java
+++ src/main/java/org/swp391_group4_backend/ecosolution/auth/domain/dto/response/UserCreationResponseDto.java
@@ -1,6 +1,5 @@
 package org.swp391_group4_backend.ecosolution.auth.domain.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
 import org.swp391_group4_backend.ecosolution.auth.domain.entity.UserRole;
 import org.swp391_group4_backend.ecosolution.auth.domain.entity.UserStatus;
 
