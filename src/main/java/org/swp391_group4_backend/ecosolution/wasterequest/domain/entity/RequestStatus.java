8b758f4cbbdc8fd83bef5184dde51c298f6ecb37
AnhNDSE190393 <evosnda34@gmail.com>
2026-03-02 02:07:40 +0700

    dependency: add mapstruct dependency streamline object mapping process

---
+++ src/main/java/org/swp391_group4_backend/ecosolution/wasterequest/domain/entity/RequestStatus.java
@@ -0,0 +1,11 @@
+package org.swp391_group4_backend.ecosolution.wasterequest.domain.entity;
+
+public enum RequestStatus {
+    PENDING,
+    ASSIGNED,
+    ACCEPTED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED,
+    REJECTED
+}
---
+++ src/main/java/org/swp391_group4_backend/ecosolution/wasterequest/domain/entity/WasteType.java
@@ -0,0 +1,12 @@
+package org.swp391_group4_backend.ecosolution.wasterequest.domain.entity;
+
+public enum WasteType {
+    ORGANIC,
+    PLASTIC,
+    PAPER,
+    GLASS,
+    METAL,
+    ELECTRONIC,
+    HAZARDOUS,
+    OTHER
+}
