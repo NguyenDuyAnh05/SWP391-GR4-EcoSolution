7d20f84b15877088879db514b95e357aec503bbb
AnhNDSE190393 <evosnda34@gmail.com>
2026-03-10 01:27:34 +0700

    remove(rest-api): remove unnecessary module written in REST_API

--- src/main/java/org/swp391_group4_backend/ecosolution/auth/domain/UserCreationRequest.java
+++
@@ -1,8 +0,0 @@
-package org.swp391_group4_backend.ecosolution.auth.domain;
-
-public record UserCreationRequest(
-        String email,
-        String password,
-        String name
-) {
-}
--- src/main/java/org/swp391_group4_backend/ecosolution/auth/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package org.swp391_group4_backend.ecosolution.auth.service;
-
-import org.swp391_group4_backend.ecosolution.auth.domain.UserCreationRequest;
-import org.swp391_group4_backend.ecosolution.auth.domain.entity.User;
-
-public interface AuthService {
-  User createUser(UserCreationRequest request);
-}
