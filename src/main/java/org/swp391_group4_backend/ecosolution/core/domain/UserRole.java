2140151ff74bd13439427471df1e11505dc439e6
AnhNDSE190393 <evosnda34@gmail.com>
2026-03-10 05:40:12 +0700

    document: add roadmap for next 12 days and document technique will be implemented

---
+++ src/main/java/org/swp391_group4_backend/ecosolution/core/domain/UserRole.java
@@ -0,0 +1,8 @@
+package org.swp391_group4_backend.ecosolution.core.domain;
+
+public enum UserRole {
+    CITIZEN,
+    ENTERPRISE,
+    ADMIN,
+    COLLECTOR
+}
---
+++ src/main/java/org/swp391_group4_backend/ecosolution/core/domain/entity/User.java
@@ -0,0 +1,17 @@
+package org.swp391_group4_backend.ecosolution.core.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+}
